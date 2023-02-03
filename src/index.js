54d9c91fde747a43eb8403281038d72d6df9d773
DmitriSerebryakov <74046834+Dmitri2121@users.noreply.github.com>
2023-02-03 16:45:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a;
+
+  let b = bracketsConfig.flat().join('').match(/.{2}/g);
+
+  for (let i = 0; i < b.length; i++) {
+    a = str;
+    str = str.replaceAll(b[i], String());
+    if (a !== str) i = -1;
+    if (str.length === 0) return true;
+  }
+  return false;
 }
